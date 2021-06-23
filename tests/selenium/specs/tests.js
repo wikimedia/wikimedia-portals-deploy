78ef495d639502918cf676acb0070b021645aee8
Jan Drewniak <jan.drewniak@gmail.com>
2021-06-23 14:06:15 +0200

    Fix selenium tests
    Test functions were unnecessarily wrapped in Mocha's `it` functions,
    which were then invoked by another `it` function.
    
    This caused the functions return value to be ignored or lost somewhere.
    
    Formatting the test functions as plain functions returns the assertion
    values correctly.
    
    This also splits the test functions for wikipedia.org across more tests.
    
    Change-Id: I0e277e0bf889f96e7705428c90602c6a52aeb0b8

--- tests/selenium/specs/tests.js
+++ tests/selenium/specs/tests.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* eslint-disable mocha/no-exports */
-
 const assert = require( 'assert' );
 
 /**
@@ -35,58 +33,43 @@ function knownErrors( message ) {
 const tests = {
 	// common tests
 	correctTitle( correctTitle ) {
-		return it( `should have the title: "${correctTitle}"`, function () {
-			const title = browser.getTitle();
-			assert.equal( title, correctTitle );
-		} );
+		const title = browser.getTitle();
+		assert.equal( title, correctTitle );
 	},
 	noBrowserErrors() {
-		return it( 'should not produce browser errors', function () {
-
-			const logs = browser.log( 'browser' ),
-				errors = logs.value.filter( ( log ) => {
-					const isError = log.level === 'SEVERE',
-						isNotaKnownError = !knownErrors( log.message );
-					if ( isError && isNotaKnownError ) {
-						return true;
-					} else {
-						return false;
-					}
-				} );
-			assert.equal( errors.length, 0, `${JSON.stringify( errors )}` );
-		} );
+		const logs = browser.log( 'browser' ),
+			errors = logs.value.filter( ( log ) => {
+				const isError = log.level === 'SEVERE',
+					isNotaKnownError = !knownErrors( log.message );
+				if ( isError && isNotaKnownError ) {
+					return true;
+				} else {
+					return false;
+				}
+			} );
+		assert.equal( errors.length, 0, `${JSON.stringify( errors )}` );
 	},
 	// Wikipedia.org specific tests
 	focusOnSearch() {
-		return it( 'search input should have focus', function () {
-			assert( browser.hasFocus( 'input[name=search]' ) );
-		} );
+		assert( browser.hasFocus( 'input[name=search]' ) );
 	},
 	expandLanguageLinks() {
-		return it( 'language list should expand', function () {
-			browser.click( '.lang-list-button' );
-			assert( browser.waitForVisible( '.lang-list-content', 1000 ) );
-		} );
+		browser.click( '.lang-list-button' );
+		assert( browser.waitForVisible( '.lang-list-content', 1000 ) );
 	},
 	collapseLanguageLinks() {
-		return it( 'language list should collapse', function () {
-			browser.click( '.lang-list-button' );
-			assert( browser.waitForVisible( '.lang-list-content', 1000, true ) );
-		} );
+		browser.click( '.lang-list-button' );
+		assert( browser.waitForVisible( '.lang-list-content', 1000, true ) );
 	},
 	searchSuggestionsForQuery( query ) {
-		return it( `search suggestions should appear for the query: ${query}`, function () {
-			browser.setValue( '#searchInput', query );
-			assert( browser.waitForVisible( '.suggestions-dropdown', 3000 ) );
-			assert( browser.waitForVisible( '.suggestion-link', 3000 ) );
-			assert( browser.getText( '.suggestion-title' ), query );
-		} );
+		browser.setValue( '#searchInput', query );
+		assert( browser.waitForVisible( '.suggestions-dropdown', 3000 ) );
+		assert( browser.waitForVisible( '.suggestion-link', 3000 ) );
+		assert( browser.getText( '.suggestion-title' ), query );
 	},
 	searchShouldRedirectTo( article ) {
-		return it( `Submitting search form should land on: ${article}`, function () {
-			browser.click( '#search-form button[type="submit"]' );
-			assert( browser.getUrl(), article );
-		} );
+		browser.click( '#search-form button[type="submit"]' );
+		assert( browser.getUrl(), article );
 	}
 };
 
--- tests/selenium/specs/wikipedia.spec.js
+++ tests/selenium/specs/wikipedia.spec.js
@@ -3,14 +3,36 @@
 const tests = require( './tests' );
 
 describe( 'www.wikipedia.org page', function () {
-	it( 'should expand language links and provide query suggestions', function () {
+
+	before( function () {
 		browser.url( 'http://localhost:8080/wikipedia.org/' );
+	} );
+
+	it( 'Should have the correct title: "Wikipedia" ', function () {
 		tests.correctTitle( 'Wikipedia' );
+	} );
+
+	it( 'should focus on search input when page is loaded', function () {
 		tests.focusOnSearch();
+	} );
+
+	it( 'should expand language links', function () {
 		tests.expandLanguageLinks();
+	} );
+
+	it( 'should collapse language links', function () {
 		tests.collapseLanguageLinks();
+	} );
+
+	it( 'should produce search suggestions for the query "Paris"', function () {
 		tests.searchSuggestionsForQuery( 'Paris' );
+	} );
+
+	it( 'should produce no console errors', function () {
 		tests.noBrowserErrors();
+	} );
+
+	it( 'Clicking on first search result should redirect to: "https://en.wikipedia.org/wiki/Paris" ', function () {
 		tests.searchShouldRedirectTo( 'https://en.wikipedia.org/wiki/Paris' );
 	} );
 } );
